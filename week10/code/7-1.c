0c94419d289a3a486fd9816ef4a143ea10ac7248
hjh1207 <1834640958@qq.com>
2019-11-11 22:46:35 -0800

    week10

---
+++ week10/code/7-1.c
@@ -0,0 +1,38 @@
+#include"my.h"
+static void callback1()
+{
+printf("-------callback1---------\n");
+}
+static void callback2()
+{
+printf("-------callback2---------\n");
+}
+static void __attribute__((constructor))before_main()
+{
+printf("------constructor--------\n");
+}
+static void __attribute__((desstructor))after_main()
+{
+printf("------desstructor--------\n");
+}
+int main()
+{
+atexit(callback1);
+atexit(callback2);
+FILE *fp;
+char buf[]={"test data"};
+if((fp=fopen("test.dat","w+"))==NULL)
+{
+perror("failed to fopen!\n ");
+return -1;
+}
+if(fputs(buf,fp)==EOF)
+{
+perror("failed to fputs!\n ");
+return -1;
+}
+printf("printf:data from line buffer");
+exit(0);
+//_exit(0);
+//return 0;
+}
---
+++ week10/code/7-3.c
@@ -0,0 +1,36 @@
+#include"my.h"
+static void callback1()
+{
+printf("-------callback1---------\n");
+}
+static void callback2()
+{
+printf("-------callback2---------\n");
+}
+static void __attribute__((constructor))before_main()
+{
+printf("------constructor--------\n");
+}
+static void __attribute__((dessstructor))after_main()
+{
+printf("------desstructor--------\n");
+}
+int main()
+{
+atexit(callback1);
+atexit(callback2);
+FILE *fp;
+char buf[]={"test data"};
+if((fp=fopen("test.dat","w+"))==NULL)
+{
+perror("failed to fopen!\n ");
+return -1;
+}
+if(fputs(buf,fp)==EOF)
+{
+perror("failed to fputs!\n ");
+return -1;
+}
+printf("printf:data from line buffer");
+return 0;
+}
