5dc6acd12cca8f47f28ceda2e1566aa6425b22ff
hjh1207 <1834640958@qq.com>
2019-12-16 20:13:10 -0800

    week14

---
+++ week14/code/class1/my.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<error.h>
+#include<wait.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<errno.h>
+#include<pthread.h>
+#include"err_exit.h"
+#include<limits.h>
+#include<assert.h>
+#define check_error(return_val,msg){\
+	if(return_val!=0){\
+		fprintf(stderr,"%s:%s\n",msg,strerror(return_val));\
+		exit(-1);\
+	}\
+}
